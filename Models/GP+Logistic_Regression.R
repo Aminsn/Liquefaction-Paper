eb8a788f23098041f0f654e73920226ef010843c
Amin Shoari Nejad <aminsh2010@gmail.com>
2021-09-17 16:38:55 +0100

    Update GP+Logistic_Regression.R

--- Models/GP+Logistic_Regression.R
+++ Models/GP+Logistic_Regression.R
@@ -1,26 +1,11 @@
 rm(list = ls())
 library(readr)
 library(R2jags)
-library(dplyr)
-library(lubridate)
 library(tidyverse)
-library(MASS)
-library(randomForest)
-
-# Running model 1 is the ultimate goal. Others are just for exploration
 
 # Reading and preparing data
-df_total_liquefaction <- read_csv("4-All.csv")
-df_total_liquefaction$status[which(df_total_liquefaction$status == "NO")] <- "No"
-df_total_liquefaction$status <- as.numeric(as.factor(df_total_liquefaction$status))
-df_total_liquefaction$status <- df_total_liquefaction$status - 1 # Making classes as 0 and 1
-
 df_total_liquefaction <- read_csv("df_total_liquefaction.csv") #Option 2: includes fines content
 df_total_liquefaction$type <- as.numeric(as.factor(df_total_liquefaction$type)) # Perhaps needs to be used as an indicator in JAGS
-
-########################################################################################################
-########################### Model 1: All variables + All observations ##################################
-########################################################################################################
 data_alt <- df_total_liquefaction
 data_alt <- data_alt %>% group_by(type) %>% 
   mutate(test_value = (test_value - mean(test_value))/sd(test_value)) %>%
@@ -33,13 +18,11 @@ data_alt <- data_alt %>% group_by(type) %>%
                   total_stress,
                   effective_stress,
                   CSR), ~ . - mean(.)))
-# Calculating imbalance difference
-x <- length(data_alt$status[which(data_alt$status == 1)]) - length(data_alt$status[which(data_alt$status == 0)])
 
-upsampling <- data_alt %>% filter(status == 0)
-samp <- sample(nrow(upsampling), 0.9 * x)
+#Mutual variables Matrix for GP
+X <- data_alt %>% dplyr::select(-status, -test_value, -CSR, -fines_content, -type)
 
-data_alt <- rbind(data_alt, upsampling[samp, ])
+################### Modelling ################### 
 
 model_code <- "
 model
@@ -47,7 +30,7 @@ model
   # Likelihood
   for (i in 1:N) {
     y[i] ~ dbin(p[i], 1)
-    logit(p[i]) <- alpha[i] + beta_5[type[i]] * x_5[i] 
+    logit(p[i]) <- alpha[i] + beta[type[i]] * x_5[i] 
                            
   }
   
@@ -64,7 +47,7 @@ model
   }
   
   for (j in 1:J) {
-  beta_5[j] ~ dnorm(a,b)
+  beta[j] ~ dnorm(a,b)
   }
   # Priors
   a ~ dnorm(0,10)
@@ -78,7 +61,6 @@ model
 }
 "
 
-X <- data_alt %>% dplyr::select(-status, -test_value, -CSR, -fines_content, -type)
 
 # Set up the data
 model_data <- list(
@@ -91,9 +73,7 @@ model_data <- list(
 )
 
 # Choose the parameters to watch
-model_parameters <- c("alpha", "beta_1", "beta_2", "beta_3", "beta_4", "beta_5", "p")
-model_parameters <- c("alpha", "beta_1", "beta_2", "beta_3", "beta_4", "beta_5", "p", "a","b")
-model_parameters <- c("alpha")
+model_parameters <- c("alpha", "beta", "tau", "rho", "sigma", "zeta")
 
 # Run the model 1
 model_run <- jags(
